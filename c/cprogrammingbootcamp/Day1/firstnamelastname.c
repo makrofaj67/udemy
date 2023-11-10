5d91d2838f518c50ea0160e67147d1de16a3d7c8
Rıdvan Akman <114802232+makrofaj67@users.noreply.github.com>
2023-11-10 21:41:50 +0300

    41

--- c/cprogrammingbootcamp/Day1/firstnamelastname.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main ()
-
-{
-    printf("\nRıdvan\n\nAkman\n30\n");
-    return 0.1;
-}
--- c/cprogrammingbootcamp/Day2/rectangleasterisks.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main () 
-
-{
-    printf("1*********\n12********\n123*******\n1234******\n12345*****\n"); //printformat
-    return 0;
-    
-}
